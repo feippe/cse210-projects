29172d675eb11cab8a7a907213758f3cf926432a
Gabriel Feippe <gabriel@feippe.com>
2024-01-13 17:23:27 -0300

    Learning Activity - C# Prep 3 - Core Requirements 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is the magic number?");
+        string magicNumberString = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberString);
+
+        Console.WriteLine("What is your guess?");
+        string guessNumberString = Console.ReadLine();
+        int guessNumber = int.Parse(guessNumberString);
+
+        if(magicNumber == guessNumber){
+            Console.WriteLine("Yes!");
+        }else if(magicNumber > guessNumber){
+            Console.WriteLine("Higher");
+        }else{
+            Console.WriteLine("Lower");
+        }
+
     }
 }
