27052e70df4310de0ac729ae9e40d2da46acec54
Gabriel Feippe <gabriel@feippe.com>
2024-01-13 16:54:49 -0300

    Learning Activity - C# Prep 2 - Part 1 and 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Please, write your percentage:");
+        string percentajeString = Console.ReadLine();
+        int percentaje = int.Parse(percentajeString);
+
+        string letter;
+
+        if (percentage >= 90){
+            letter = "A";
+        }else if (percentage >= 80){
+            letter = "B";
+        }else if (percentage >= 70){
+            letter = "C";
+        }else if (percentage >= 60){
+            letter = "D";
+        }else {
+            letter = "F"
+        }
+
+        if( percentage >= 70){
+            Console.WriteLine($"Your grade is {letter}! Congratulations! You approved the semester.");
+        }else{
+            Console.WriteLine($"Your grade is {letter}. Sorry, You need to perform the semester again.");
+        }
+
     }
 }
