bf31eef9b574c3c0fe9fe8f01ff508929f12b010
Gabriel Feippe <gabriel@feippe.com>
2024-01-13 17:40:04 -0300

    Learning Activity - C# Prep 3 - Stretch Challenge 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,12 +10,15 @@ class Program
         //string magicNumberString = Console.ReadLine();
         //int magicNumber = int.Parse(magicNumberString);
 
+        int guesses = 0;
+
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 100);
 
         Console.WriteLine("What is your guess?");
         string guessNumberString = Console.ReadLine();
         int guessNumber = int.Parse(guessNumberString);
+        guesses = 1;
 
         while (magicNumber != guessNumber){
             if(magicNumber > guessNumber){
@@ -27,9 +30,15 @@ class Program
             Console.WriteLine("What is your guess?");
             guessNumberString = Console.ReadLine();
             guessNumber = int.Parse(guessNumberString);
+            guesses += 1;
         }
 
-        Console.WriteLine("Yes!");
+        Console.WriteLine("YES!");
+        if(guesses>1){
+            Console.WriteLine($"You guess in {guesses} attempts!");
+        }else{
+            Console.WriteLine($"You guess in 1 attempt!");
+        }
 
     }
 }
