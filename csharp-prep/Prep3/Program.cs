204e93f430cb72a9342545278f599a8aa72a1c2f
Gabriel Feippe <gabriel@feippe.com>
2024-01-13 17:47:30 -0300

    Learning Activity - C# Prep 3 - Stretch Challenge 2 (final)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,40 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Prep3 World!");
 
-        //Console.WriteLine("What is the magic number?");
-        //string magicNumberString = Console.ReadLine();
-        //int magicNumber = int.Parse(magicNumberString);
+        bool play = true;
 
-        int guesses = 0;
+        while (play){
+            int guesses = 0;
 
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
 
-        Console.WriteLine("What is your guess?");
-        string guessNumberString = Console.ReadLine();
-        int guessNumber = int.Parse(guessNumberString);
-        guesses = 1;
-
-        while (magicNumber != guessNumber){
-            if(magicNumber > guessNumber){
-                Console.WriteLine("Higher");
-            }else{
-                Console.WriteLine("Lower");
-            }
-            
             Console.WriteLine("What is your guess?");
-            guessNumberString = Console.ReadLine();
-            guessNumber = int.Parse(guessNumberString);
-            guesses += 1;
-        }
+            string guessNumberString = Console.ReadLine();
+            int guessNumber = int.Parse(guessNumberString);
+            guesses = 1;
 
-        Console.WriteLine("YES!");
-        if(guesses>1){
-            Console.WriteLine($"You guess in {guesses} attempts!");
-        }else{
-            Console.WriteLine($"You guess in 1 attempt!");
+            while (magicNumber != guessNumber){
+                if(magicNumber > guessNumber){
+                    Console.WriteLine("Higher");
+                }else{
+                    Console.WriteLine("Lower");
+                }
+                
+                Console.WriteLine("What is your guess?");
+                guessNumberString = Console.ReadLine();
+                guessNumber = int.Parse(guessNumberString);
+                guesses += 1;
+            }
+
+            Console.WriteLine("YES!");
+            if(guesses>1){
+                Console.WriteLine($"You guess in {guesses} attempts!");
+            }else{
+                Console.WriteLine($"You guess in 1 attempt!");
+            }
+
+            Console.WriteLine("Do you want to play again? (write 'yes' if you want to play again)");
+            string playAgain = Console.ReadLine();
+
+            if(playAgain.ToLower() != "yes"){
+                play = false;
+            }
         }
 
     }
