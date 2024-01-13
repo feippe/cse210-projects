123e46e359e5ba2a297de5c1b5cb1daa0fd3a571
Gabriel Feippe <gabriel@feippe.com>
2024-01-13 18:06:59 -0300

    Learning Activity - C# Prep 4 - Core Requirements 1, 2 and 3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        Console.Write("Enter number: ");
+        string numberString = Console.ReadLine();
+        int theNumber = int.Parse(numberString);
+
+        while (theNumber > 0){
+            numbers.Add(theNumber);
+            Console.Write("Enter number: ");
+            numberString = Console.ReadLine();
+            theNumber = int.Parse(numberString);
+        }
+
+        int sum = 0;
+        double average = 0;
+        int largest = 0;
+
+        foreach(int number in numbers){
+            sum += number;
+            if(number > largest){
+                largest = number;
+            }
+        }
+        average = sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
