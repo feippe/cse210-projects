fc53898d53e4930c4b265ff236636eeccf6be9d8
Gabriel Feippe <gabriel@feippe.com>
2024-01-27 14:22:18 -0300

    Week 03 Develop: Scripture Memorizer (exceed requirements)

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,14 +1,23 @@
 using System;
 
-//Exceed the requirements: I added a counter for hidden words and show them on the screen for the user.
+/*
+Exceed the requirements: 
+1. I added a counter for hidden words and show them on the screen for the user.
+2. I put all the scripts in a txt file and the program picks them up from there, and takes one of them at random.
+*/
 
 class Program 
 {
     static void Main(string[] args)
     {
         Random random = new Random();
-        Reference reference = new Reference("1 Nephi", 3, 7);
-        Scripture scripture = new Scripture(reference, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+        
+        string[] lines = System.IO.File.ReadAllLines("scripture-base.txt");
+        string line = lines[random.Next(lines.Count())];
+        string[] parts = line.Split("|");
+
+        Reference reference = new Reference(parts[0], int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3]));
+        Scripture scripture = new Scripture(reference, parts[4]);
 
         Console.Clear();
         Console.WriteLine(reference.GetDisplayReference());
@@ -31,6 +40,7 @@ class Program
             Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             command = Console.ReadLine();
         }
-
+        
     }
+
 }
