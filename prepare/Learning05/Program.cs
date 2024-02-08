d05be99428ee2d4a5f7e9a9658d9d49a7f256b8e
Gabriel Feippe <gabriel@feippe.com>
2024-02-08 19:59:49 -0300

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square square = new Square("Blue",25);
+        Rectangle rectangle = new Rectangle("Yellow",15,35);
+        Circle circle = new Circle("Red", 30);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes){
+            Console.WriteLine($"{shape.GetColor()} - {shape.GetArea()}");
+        }
+
+
+
+
     }
 }
